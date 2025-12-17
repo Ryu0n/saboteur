5874365580883b9e70bdb84ebcefeab121e9e09f
Ryu0n <hsung951027@gmail.com>
2025-12-17 16:45:19 +0900

    ✅ Add additional test cases

--- tests/test_saboteur_mutations.py
+++ tests/test_saboteur_mutations.py
@@ -1,8 +1,13 @@
 import logging
 import pytest
+
+from typing import List
+
 from saboteur.application.facade import Saboteur
 from saboteur.infrastructure.strategies.injections import NullInjectionStrategy
 from saboteur.infrastructure.strategies.flippings import TypeFlipStrategy
+from saboteur.domain.mutation.strategies import MutationStrategy
+from saboteur.domain.mutation.configs import MutationConfig
 
 
 @pytest.fixture
@@ -19,17 +24,42 @@ def mock_data():
     }
 
 @pytest.mark.parametrize(
-    "strategies",
+    "strategies, apply_all_strategies, apply_all_keys, num_strategies, num_keys",
     [
-        [NullInjectionStrategy(),],
-        [TypeFlipStrategy(),],
-        [NullInjectionStrategy(), TypeFlipStrategy()],
+        ([NullInjectionStrategy(),], True, True, None, None),
+        ([TypeFlipStrategy(),], True, True, None, None),
+        ([NullInjectionStrategy(), TypeFlipStrategy()], True, True, None, None),
+        ([NullInjectionStrategy(),], True, False, None, 1),
+        ([TypeFlipStrategy(),], True, False, None, 1),
+        ([NullInjectionStrategy(), TypeFlipStrategy()], True, False, None, 1),
+        ([NullInjectionStrategy(),], False, True, 1, None),
+        ([TypeFlipStrategy(),], False, True, 1, None),
+        ([NullInjectionStrategy(), TypeFlipStrategy()], False, True, 1, None),
+        ([NullInjectionStrategy(),], False, False, 1, 1),
+        ([TypeFlipStrategy(),], False, False, 1, 1),
+        ([NullInjectionStrategy(), TypeFlipStrategy()], False, False, 1, 1),
     ]
 )
-def test_saboteur_mutations(strategies, mock_data, logger):
+def test_saboteur_mutations(
+    strategies: List[MutationStrategy],
+    apply_all_strategies: bool,
+    apply_all_keys: bool,
+    num_strategies: int,
+    num_keys: int,
+    mock_data,
+    logger,
+):
     logger.debug(f"Testing with strategies: {strategies}")
     
-    saboteur = Saboteur(strategies=strategies)
+    config = MutationConfig(
+        strategies=strategies,
+        apply_all_strategies=apply_all_strategies,
+        apply_all_keys=apply_all_keys,
+        num_strategies=num_strategies,
+        num_keys=num_keys,
+    )
+    
+    saboteur = Saboteur(config=config)
     mutated_data = saboteur.attack(mock_data)
     
     logger.debug(f"Original data: {mock_data}")
